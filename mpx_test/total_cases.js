808e5a75f2ed683548bc0849914edf47c3c4be66
Morgan Mueller <84867869+NYC-DOHMH-MM@users.noreply.github.com>
2022-08-01 10:30:10 -0400

    Update total_cases.js

--- mpx_test/total_cases.js
+++ mpx_test/total_cases.js
@@ -6,7 +6,7 @@ getData();
 Pull in monkeypox total case numbers; clean the data; return elements for the data values to be added to the DOM
 */
 async function getData() {
-    const response = await fetch('https://raw.githubusercontent.com/nychealth/covid-maps/main/mpx_test/total-daily-cases.csv');
+    const response = await fetch('https://raw.githubusercontent.com/nychealth/monkeypox-data/main/totals/total-daily-cases.csv');
     const data = await response.text();
     const table = data.split('\n').slice(1,2);
 
